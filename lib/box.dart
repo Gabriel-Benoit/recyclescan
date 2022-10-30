3e9eb5d91d02b8861220b258ba37360972f2b253
Gabriel <56794652+gabenoit@users.noreply.github.com>
2022-10-30 12:14:58 +0100

    improving architecture

--- lib/box.dart
+++ lib/box.dart
@@ -2,82 +2,99 @@ import 'package:flutter/material.dart';
 
 /// Widget Wrapper représentant une box qui identifie un déchet et
 /// qui permet d'afficher une description qui donne une directive de tri
-class BoxWithBorder extends StatelessWidget {
-  BoxWithBorder({super.key, required this.height, required this.width, required this.posX, required this.posY}){
-    description = const WasteDescription();
-  }
+class BoxWithBorder extends StatefulWidget {
+  const BoxWithBorder(
+      {super.key,
+      required this.height,
+      required this.width,
+      required this.posX,
+      required this.posY});
+
   final double height;
   final double width;
   final double posX;
   final double posY;
-  late final WasteDescription description;
+
+  @override
+  State<StatefulWidget> createState() => _BoxWithBorderState();
+}
+
+class _BoxWithBorderState extends State<BoxWithBorder> {
+  bool isDescOpen = false;
+
+  void _toggleDescription() {
+    setState(() {
+      isDescOpen = !isDescOpen;
+    });
+  }
 
   @override
   Widget build(BuildContext context) {
-    return Positioned(
-      left: posX,
-      top: posY,
-      child: SizedBox(
-        height: height,
-        width: width,
-        child: Container(
-          decoration: BoxDecoration(
-            border: Border.all(color: Colors.green, width: 2.0),
-          ),
-          child: Opacity(
-            opacity: 1.0,
-            child: Box(description: description,),
+    if (!isDescOpen) {
+      return Positioned(
+        left: widget.posX,
+        top: widget.posY,
+        child: SizedBox(
+          height: widget.height,
+          width: widget.width,
+          child: Container(
+            decoration: BoxDecoration(
+              border: Border.all(color: Colors.green, width: 2.0),
+            ),
+            child: Box(
+              parent: widget,
+              onPressed: _toggleDescription,
+            ),
           ),
         ),
-      ),
-    );
+      );
+    } else {
+      return Container(
+        decoration: BoxDecoration(
+          border: Border.all(color: Colors.white, width: 2.0),
+        ),
+        child: WasteDescription(
+          closeCallBack: _toggleDescription,
+        ),
+      );
+    }
   }
 }
 
 class Box extends StatefulWidget {
-  const Box({super.key, required this.description });
-  final WasteDescription description;
+  const Box({super.key, required this.parent, required this.onPressed});
+
+  final void Function() onPressed;
+  final BoxWithBorder parent;
   @override
   State<StatefulWidget> createState() => _BoxState();
 }
 
 class _BoxState extends State<Box> {
-  bool _isDescriptionOpened = false;
-  _openDescription(){
-    setState(() {
-      _isDescriptionOpened = !_isDescriptionOpened;
-      if(_isDescriptionOpened){
-        widget.description.open();
-      } else {
-        widget.description.close();
-      }
-    });
-  }
   @override
   Widget build(BuildContext context) {
-    return GestureDetector(onTap: _openDescription);
+    return GestureDetector(onTap: widget.onPressed);
   }
 }
 
-class WasteDescription extends StatefulWidget {
-  const WasteDescription({super.key});
-  open(){
-    print("Opening description");
-  }
-  close(){
-    print("Closing description");
-  }
-  
-  @override
-  State<StatefulWidget> createState() => _WasteDescriptionState();
+class WasteDescription extends StatelessWidget {
+  WasteDescription({super.key, required this.closeCallBack});
 
-}
-
-class _WasteDescriptionState extends State<WasteDescription> {
+  void Function() closeCallBack;
   @override
   Widget build(BuildContext context) {
-    // TODO: implement build
-    throw UnimplementedError();
+    return Container(
+      decoration: BoxDecoration(color: Colors.white),
+      child: Stack(
+        children: [
+          Positioned(
+            child: CloseButton(
+              onPressed: closeCallBack,
+              color: Colors.deepOrange,
+            ),
+          ),
+        ],
+      ),
+    );
   }
-  
 }
