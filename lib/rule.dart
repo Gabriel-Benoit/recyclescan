6fe3768662eddf55446da84f26eee7344c89ac17
Gabriel <56794652+gabenoit@users.noreply.github.com>
2022-11-03 10:04:02 +0100

    feature - garbage description

--- lib/rule.dart
+++ lib/rule.dart
@@ -1,14 +1,14 @@
+import 'package:flutter/painting.dart';
 import 'dart:collection';
-import 'dart:ui';
 
 import 'package:recyclescan/garbage.dart';
 
 class Rule {
   final String name;
-  final String imageUrl;
+  final ImageProvider image;
   final Color color;
 
-  const Rule({required this.name, required this.imageUrl, required this.color});
+  const Rule({required this.name, required this.image, required this.color});
 
   static Map<Garbage, Rule> fromJSON(String path) {
     return LinkedHashMap.identity();
---
+++ lib/wastedescription.dart
@@ -0,0 +1,49 @@
+import 'dart:io';
+
+import 'package:flutter/material.dart';
+
+import 'garbage.dart';
+import 'rule.dart';
+
+class WasteDescription extends StatelessWidget {
+  final Garbage garbage;
+  final Rule rule;
+  final void Function() closeCallBack;
+
+  const WasteDescription(
+      {super.key,
+      required this.garbage,
+      required this.rule,
+      required this.closeCallBack});
+
+  @override
+  Widget build(BuildContext context) {
+    Size size = MediaQuery.of(context).size;
+
+    return Container(
+      width: size.width,
+      height: size.height,
+      decoration: BoxDecoration(color: rule.color),
+      child: Stack(
+        children: [
+          Positioned(
+            top: 0,
+            left: 0,
+            child: CloseButton(
+              onPressed: closeCallBack,
+              color: Colors.deepOrange,
+            ),
+          ),
+          Center(
+            child: Column(mainAxisSize: MainAxisSize.min, children: [
+              Text("Déchet identifié: ${garbage.name}"),
+              Image(image: garbage.image, semanticLabel: "Garbage picture"),
+              Text("Politique de tri: ${rule.name}"),
+              Image(image: rule.image, semanticLabel: "Rule picture"),
+            ]),
+          )
+        ],
+      ),
+    );
+  }
+}
