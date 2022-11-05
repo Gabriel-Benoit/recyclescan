cd3e808d37682cd4c438c2dc05da0d6e21731d76
valatt19 <50866841+valatt19@users.noreply.github.com>
2022-11-05 19:11:56 +0100

    Json (#1)
    * fromJSON implemented
    
    * fct fromJSON tested
    
    * double json files

--- lib/city.dart
+++ lib/city.dart
@@ -4,11 +4,11 @@ import 'package:recyclescan/rule.dart';
 class City {
   final int postalCode;
   final String name;
-  final Map<Garbage, Rule> rules;
+  final Future<Map<Garbage, Rule>> rules;
 
   const City({required this.postalCode, required this.name, required this.rules});
 }
 
 final List<City> cities = [
-  City(name: "Namur", postalCode: 5000, rules: Rule.fromJSON("assets/Namur.json"))
+  City(name: "Namur", postalCode: 5000, rules: Rule.fromJSON("assets/Namur_garbages.json", "assets/Namur_rules.json"))
 ];
--- lib/rule.dart
+++ lib/rule.dart
@@ -1,6 +1,10 @@
+import 'dart:convert';
 import 'package:flutter/painting.dart';
 import 'dart:collection';
 
+
+import 'package:flutter/material.dart';
+import 'package:flutter/services.dart' show rootBundle;
 import 'package:recyclescan/garbage.dart';
 
 class Rule {
@@ -10,7 +14,38 @@ class Rule {
 
   const Rule({required this.name, required this.image, required this.color});
 
-  static Map<Garbage, Rule> fromJSON(String path) {
-    return LinkedHashMap.identity();
+  static Future<Map<Garbage, Rule>> fromJSON(String garbagesPath, String rulesPath)  async {
+    // Load data from json file and wait for result
+    String data = await rootBundle.loadString(garbagesPath);
+    final jsonGarbages = json.decode(data);
+
+    String data2 = await rootBundle.loadString(rulesPath);
+    final jsonRules = json.decode(data2);
+
+    // Init and fill rules map
+    Map<Garbage, Rule> rules = {};
+    for(var i = 0 ; i < garbages.length;i++) {
+      // Find corresponding garbage
+      Garbage gb = garbages[jsonGarbages[i]["garbage_id"]];
+      var ruleId = jsonGarbages[i]["rule_id"];
+
+      // Match correct color (String -> Color)
+      String colStr = jsonRules[ruleId]["color"];
+      Color color;
+      switch(colStr) {
+        case "blue": {color = Colors.blue;}
+          break;
+        case "yellow": {color = Colors.yellow;}
+          break;
+        case "green": {color = Colors.green;}
+          break;
+        default:{color = Colors.grey;}
+          break;
+      }
+      // Add new rule in the map
+      rules[gb] = Rule(name: jsonRules[ruleId]["name"], image: NetworkImage(jsonRules[ruleId]["imageUrl"]), color: color);
+    }
+
+    return rules;
   }
 }
