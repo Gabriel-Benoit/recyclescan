0d9e5d53b171d4d4e0cdb6b1eea9e7ecc1f369b2
Gabriel <56794652+gabenoit@users.noreply.github.com>
2022-11-13 00:29:47 +0100

    feature/province selection page

--- lib/main.dart
+++ lib/main.dart
@@ -1,6 +1,7 @@
 import 'package:flutter/material.dart';
 import 'package:camera/camera.dart';
-
+import 'package:recyclescan/location/locationpage.dart';
+import 'package:shared_preferences/shared_preferences.dart';
 import 'city.dart';
 import 'detector/home.dart';
 import 'utils/placeholder.dart';
@@ -9,22 +10,29 @@ Future<void> main() async {
   WidgetsFlutterBinding.ensureInitialized();
   initCities();
   final cameras = await availableCameras();
-
+  final prefs = await SharedPreferences.getInstance();
+  //await prefs.remove("location");
   final camera = cameras.first;
 
-  runApp(MaterialApp(
-    title: 'RecycleScan',
-    theme: ThemeData(
-      primarySwatch: Colors.green,
-      bottomAppBarColor: Colors.green,
+  runApp(
+    MaterialApp(
+      title: 'RecycleScan',
+      theme: ThemeData(
+        primarySwatch: Colors.green,
+        bottomAppBarColor: Colors.green,
+      ),
+      home: WidgetManager(
+        camera: camera,
+        prefs: prefs,
+      ),
     ),
-    home: WidgetManager(camera: camera),
-  ));
+  );
 }
 
 class WidgetManager extends StatefulWidget {
   final CameraDescription camera;
-  const WidgetManager({super.key, required this.camera});
+  final SharedPreferences prefs;
+  const WidgetManager({super.key, required this.camera, required this.prefs});
 
   @override
   State<StatefulWidget> createState() => _WidgetManagerState();
@@ -34,17 +42,27 @@ class _WidgetManagerState extends State<WidgetManager> {
   Widget _currentWidget = Container();
   final _tileStyle = ListTileStyle.list;
 
-  void _setWidget(Widget w){
+  void _setWidget(Widget w, {bool popCtx = true}) {
     setState(() {
       _currentWidget = w;
     });
-    Navigator.pop(context);
+    if (popCtx) {
+      Navigator.pop(context);
+    }
   }
 
   @override
   void initState() {
     super.initState();
-    _currentWidget = HomePage(camera: widget.camera);
+    if (widget.prefs.getStringList("location") == null) {
+      _currentWidget = LocationPage(
+        okHandler: () =>
+            _setWidget(HomePage(camera: widget.camera), popCtx: false),
+        prefs: widget.prefs,
+      );
+    } else {
+      _currentWidget = HomePage(camera: widget.camera);
+    }
   }
 
   @override
@@ -64,25 +82,32 @@ class _WidgetManagerState extends State<WidgetManager> {
               title: const Text("Camera"),
               style: _tileStyle,
               textColor: Colors.lightGreen,
-              onTap: (() =>
-                 _setWidget(HomePage(camera: widget.camera))
-              ),
+              onTap: (() => _setWidget(HomePage(camera: widget.camera))),
             ),
             ListTile(
               title: const Text("Signaler un déchet mal identifié"),
               style: _tileStyle,
               textColor: Colors.lightGreen,
-              onTap: (() =>
-                  _setWidget(const PlaceHolder())
-              ),
+              onTap: (() => _setWidget(const PlaceHolder())),
             ),
             ListTile(
               title: const Text("A propos de nous"),
               style: _tileStyle,
               textColor: Colors.lightGreen,
-              onTap: (() =>
-                  _setWidget(const PlaceHolder())
-              ),
+              onTap: (() => _setWidget(const PlaceHolder())),
+            ),
+            ListTile(
+              title: const Text("Changer ma province"),
+              style: _tileStyle,
+              textColor: Colors.lightGreen,
+              onTap: (() => _setWidget(
+                    LocationPage(
+                      okHandler: () => _setWidget(
+                          HomePage(camera: widget.camera),
+                          popCtx: false),
+                      prefs: widget.prefs,
+                    ),
+                  )),
             )
           ],
         ),
