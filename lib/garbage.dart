e7c36f561c1de3fa8acdd51ad654982b62fe93a7
Gabriel <56794652+gabenoit@users.noreply.github.com>
2022-11-20 21:33:43 +0100

    formatting (flutter formatter)

--- lib/garbage.dart
+++ lib/garbage.dart
@@ -7,38 +7,52 @@ class Garbage {
   const Garbage({required this.name, required this.image});
 }
 
-final Map<String,Garbage> garbages = {
-  "can": const Garbage(name: "Canette",
+final Map<String, Garbage> garbages = {
+  "can": const Garbage(
+      name: "Canette",
       image: NetworkImage(
           "https://upload.wikimedia.org/wikipedia/commons/6/6c/Tolkki20091027.jpg")),
-  "bottle": const Garbage(name: "Bouteille",
+  "bottle": const Garbage(
+      name: "Bouteille",
       image: NetworkImage(
           "https://upload.wikimedia.org/wikipedia/commons/e/e5/Evian_Bottle.jpg")),
-  "glassbottle": const Garbage(name: "Bouteille en verre",
+  "glassbottle": const Garbage(
+      name: "Bouteille en verre",
       image: NetworkImage(
           "https://upload.wikimedia.org/wikipedia/commons/0/03/Green_glass_bottle_with_RFID_chip_Lagavulin_distillery-3192.jpg")),
-  "pizzabox": const Garbage(name: "Carton à pizza",
+  "pizzabox": const Garbage(
+      name: "Carton à pizza",
       image: NetworkImage(
           "https://upload.wikimedia.org/wikipedia/commons/6/64/2020-02-21_12_39_16_Pizza_box_from_Buon_Appetito%27s_NY_Pizza_in_Dulles%2C_Loudoun_County%2C_Virginia.jpg")),
-  "toothpaste": const Garbage(name: "Tube de dentifrice",
+  "toothpaste": const Garbage(
+      name: "Tube de dentifrice",
       image: NetworkImage(
           "https://upload.wikimedia.org/wikipedia/commons/3/34/Charcoal_Toothpaste.jpg")),
-  "teabag": const Garbage(name: "Sachet de thé",
+  "teabag": const Garbage(
+      name: "Sachet de thé",
       image: NetworkImage(
           "https://upload.wikimedia.org/wikipedia/commons/e/e1/Tea_bag_777.jpg")),
-  "paperteabag": const Garbage(name: "Sachet de thé en papier",
+  "paperteabag": const Garbage(
+      name: "Sachet de thé en papier",
       image: NetworkImage(
           "https://upload.wikimedia.org/wikipedia/commons/e/e1/Tea_bag_777.jpg")),
 };
 
 final Map<Garbage, Map<String, Garbage>> alternatives = {
-  garbages["bottle"]!:{"Cette bouteille est en verre ?":garbages["glassbottle"]!},
-  garbages["teabag"]!:{"Cette sachet est en papier ?":garbages["paperteabag"]!}
-
+  garbages["bottle"]!: {
+    "Cette bouteille est en verre ?": garbages["glassbottle"]!
+  },
+  garbages["teabag"]!: {
+    "Cette sachet est en papier ?": garbages["paperteabag"]!
+  }
 };
 final Map<Garbage, List<String>> comments = {
   garbages["toothpaste"]!: ["Le tube doit être vide."],
-  garbages["bottle"]!: ["Si la bouteille contient des produits corrosifs et-ou toxiques, elle se trie au recyparc - DSM (déchets spéciaux des ménages)"],
-  garbages["glassbottle"]!: ["Les bouchons de bouteille de vin, de champagne et de cidre se trient au recyparc - bouchons de liège",
-                             "Les capsules métalliques se trient dans le sac bleu des PMC"]
+  garbages["bottle"]!: [
+    "Si la bouteille contient des produits corrosifs et-ou toxiques, elle se trie au recyparc - DSM (déchets spéciaux des ménages)"
+  ],
+  garbages["glassbottle"]!: [
+    "Les bouchons de bouteille de vin, de champagne et de cidre se trient au recyparc - bouchons de liège",
+    "Les capsules métalliques se trient dans le sac bleu des PMC"
+  ]
 };
--- lib/rule.dart
+++ lib/rule.dart
@@ -2,7 +2,6 @@ import 'dart:convert';
 import 'package:flutter/painting.dart';
 import 'dart:collection';
 
-
 import 'package:flutter/material.dart';
 import 'package:flutter/services.dart' show rootBundle;
 import 'package:recyclescan/garbage.dart';
@@ -14,7 +13,8 @@ class Rule {
 
   const Rule({required this.name, required this.image, required this.color});
 
-  static Future<Map<Garbage, Rule>> fromJSON(String garbagesPath, String rulesPath)  async {
+  static Future<Map<Garbage, Rule>> fromJSON(
+      String garbagesPath, String rulesPath) async {
     // Load data from json file and wait for result
     String data = await rootBundle.loadString(garbagesPath);
     final jsonGarbages = json.decode(data);
@@ -24,7 +24,7 @@ class Rule {
 
     // Init and fill rules map
     Map<Garbage, Rule> rules = {};
-    for(var i = 0 ; i < garbages.length;i++) {
+    for (var i = 0; i < garbages.length; i++) {
       // Find corresponding garbage
       Garbage gb = garbages[jsonGarbages[i]["garbage_id"]]!;
       var ruleId = jsonGarbages[i]["rule_id"];
@@ -32,18 +32,33 @@ class Rule {
       // Match correct color (String -> Color)
       String colStr = jsonRules[ruleId]["color"];
       Color color;
-      switch(colStr) {
-        case "blue": {color = Colors.blue;}
+      switch (colStr) {
+        case "blue":
+          {
+            color = Colors.blue;
+          }
           break;
-        case "yellow": {color = Colors.yellow;}
+        case "yellow":
+          {
+            color = Colors.yellow;
+          }
           break;
-        case "green": {color = Colors.green;}
+        case "green":
+          {
+            color = Colors.green;
+          }
           break;
-        default:{color = Colors.grey;}
+        default:
+          {
+            color = Colors.grey;
+          }
           break;
       }
       // Add new rule in the map
-      rules[gb] = Rule(name: jsonRules[ruleId]["name"], image: NetworkImage(jsonRules[ruleId]["imageUrl"]), color: color);
+      rules[gb] = Rule(
+          name: jsonRules[ruleId]["name"],
+          image: NetworkImage(jsonRules[ruleId]["imageUrl"]),
+          color: color);
     }
 
     return rules;
